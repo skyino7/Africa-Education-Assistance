b644f621091ff43cd1a101863091652840d53cc8
Seth Ketsi <christambassador12@gmail.com>
2024-06-24 10:30:33 +0000

    Removed the routes and middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const dbConnect = require('./config/db');
 const cors = require('cors');
 const clientRoutes = require('./routes/client');
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
+const protectedRoutes = require('./routes/protectedRoutes');
 
 const app = express();
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
 app.use(express.json());
 
 // Connect to MongoDB
@@ -16,5 +18,6 @@ dbConnect();
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/client', clientRoutes);
+app.use('/protected', protectedRoutes);
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
