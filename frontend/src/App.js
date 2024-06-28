e0557535fa35f43d8645ae9af6bb8e5d768645d4
Seth Ketsi <christambassador12@gmail.com>
2024-06-28 23:25:56 +0000

    Redux implemented to handle users state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,42 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
+import { Toaster } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { updateUser } from './redux/AuthSlice';
+import UserLayouts from './layouts/UserLayouts';
+import AdminLayouts from './layouts/AdminLayouts';
 
 function App() {
+
+  const user = useSelector((state) => state.auth.user);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(updateUser());
+  }, [user, dispatch]);
+
   return (
     <Router>
+      <Toaster />
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+
+          <Route path='/user' element={<UserLayouts />}>
+            <Route index element={<Home />} />
+          </Route>
+
+          <Route path='/admin' element={<AdminLayouts />}>
+            <Route index element={<Admin />} />
+          </Route>
+
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+
+          <Route path="/home" element={<Home />} />
+
         </Routes>
       </div>
     </Router>
